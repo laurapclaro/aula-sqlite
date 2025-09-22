477e270e1894e4c4e0f684a892d907bba55e58cd
Laura Papa <laurapapa693@gmail.com>
2025-09-22 10:21:47 -0300

    função de update

--- main.py
+++ main.py
@@ -1,52 +1,67 @@
 #criando e conectando com banco de dados
 import sqlite3
 
-#Criar uma conexão com banco de dados chamado de "escola.db"
+# #Criar uma conexão com banco de dados chamado de "escola.db"
 conexao = sqlite3.connect("escola.db")
 
-#Criamos um obejto chamado de cursor e vai servir para executar os comandos SQL
+# #Criamos um obejto chamado de cursor e vai servir para executar os comandos SQL
 cursor = conexao.cursor()
 
 
-#Criando uma table no banco chamada de "alunos"
+# #Criando uma table no banco chamada de "alunos"----------------------
+
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS alunos (
+#      id INTEGER PRIMARY KEY AUTOINCREMENT,
+#      nome TEXT NOT NULL,
+#      idade INTEGER,
+#      curso TEXT          
+#     )
+# """)
+# print ("Table criada!")
+
+
+# #Inserindo um valor no banco de dados-------------------------------
+
+# cursor.execute("""
+# INSERT INTO alunos (nome, idade, curso)          
+# VALUES (?, ?, ?)           
+               
+# """, 
+# ("Laura", 24, "Engenharia")            
+               
+#                )
+
+# lista_alunos = [
+#     ("Raul", 20, "Direito"),
+#     ("Kazuki", 22, "Cozinheiro"),
+#     ("Damas", 33, "Programador")
+# ]
+
+# #Executemany permite inserir multiplas linhas----------------------------
+
+# cursor.executemany("""
+# INSERT INTO alunos (nome, idade, curso)          
+# VALUES (?, ?, ?)  
+#                """,
+# lista_alunos               )
+
+
+# #confirmar as alteraçõesdo banco----------------------
+# conexao.commit()
+# print("Dados onseridos com sucesso!")
+
+
+#atualizar dados tabela alunos---------------------
+#atualizar o nome de um aluno
+#Sempre usar WHERE, senao todos os registros serao alterados
+
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-     id INTEGER PRIMARY KEY AUTOINCREMENT,
-     nome TEXT NOT NULL,
-     idade INTEGER,
-     curso TEXT          
-    )
-""")
-print ("Table criada!")
+UPDATE alunos
+SET nome = ?, curso = ?
+WHERE id = ?
+""", ("Felipe", "Ads", 2))
 
-
-#Inserindo um valor no banco de dados
-
-cursor.execute("""
-INSERT INTO alunos (nome, idade, curso)          
-VALUES (?, ?, ?)           
-               
-""", 
-("Laura", 24, "Engenharia")            
-               
-               )
-
-lista_alunos = [
-    ("Raul", 20, "Direito"),
-    ("Kazuki", 22, "Cozinheiro"),
-    ("Damas", 33, "Programador")
-]
-
-#Executemany permite inserir multiplas linhas
-
-cursor.executemany("""
-INSERT INTO alunos (nome, idade, curso)          
-VALUES (?, ?, ?)  
-               """,
-lista_alunos               )
-
-
-#confirmar as alteraçõesdo banco
 conexao.commit()
-print("Dados onseridos com sucesso!")
+print ("Dados alterados com sucesso!")
